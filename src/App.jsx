9a1907477bfff65d06a3e0abc550c6091d18e324
tuiusx <tuiusx@icloud.com>
2023-02-24 20:35:02 -0300

    adding css modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import { Post } from "./Post"
+import { Header } from "./components/Header"
+
+import './styles.css'
 
 export function App() {
 
   return (
     <>
+      <Header />
       <Post 
         author="Ricador martins" 
         content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit consectetur perferendis possimus pariatur sequi recusandae? Corporis fugit aliquid pariatur veritatis, ea sed dolore rerum nisi consequuntur nostrum, a doloribus recusandae!" 

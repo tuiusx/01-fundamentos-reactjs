817a6d040d947735fa3a4665c56b6fa01ba4103c
tuiusx <tuiusx@icloud.com>
2023-02-24 19:41:39 -0300

    add propriedades

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,16 @@ export function App() {
 
   return (
     <>
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      <Post 
+        author="Ricador martins" 
+        content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit consectetur perferendis possimus pariatur sequi recusandae? Corporis fugit aliquid pariatur veritatis, ea sed dolore rerum nisi consequuntur nostrum, a doloribus recusandae!" 
+      />
+      <Post 
+        author="tiao" 
+        content="biscoito" 
+      />
+
     </>
   )
 }
+

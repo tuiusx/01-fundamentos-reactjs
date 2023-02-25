b7556ab15ef80edb7b91807bd2dd0c12394b352f
tuiusx <tuiusx@icloud.com>
2023-02-25 15:25:23 -0300

    Application structure - Component: Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,32 @@
 import { Post } from "./Post"
+
 import { Header } from "./components/Header"
+import { Sidebar } from "./components/Sidebar"
+
+import styles from './App.module.css'
 
 import './global.css'
 
 export function App() {
 
   return (
-    <>
+    <div>
       <Header />
-      <Post 
-        author="Ricador martins" 
-        content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit consectetur perferendis possimus pariatur sequi recusandae? Corporis fugit aliquid pariatur veritatis, ea sed dolore rerum nisi consequuntur nostrum, a doloribus recusandae!" 
-      />
-      <Post 
-        author="tiao" 
-        content="biscoito" 
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+            <Post 
+              author="Ricador martins" 
+              content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit consectetur perferendis possimus pariatur sequi recusandae? Corporis fugit aliquid pariatur veritatis, ea sed dolore rerum nisi consequuntur nostrum, a doloribus recusandae!" 
+            />
+            <Post 
+              author="tiao" 
+              content="biscoito" 
+            />
+        </main>
+      </div>
 
-    </>
+    </div>
   )
 }
 

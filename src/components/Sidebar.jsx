e8b451379f286f3d627f7d91b0408c14305491da
tuiusx <tuiusx@icloud.com>
2023-02-25 16:08:58 -0300

    Application structure - Finishing Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,5 @@
+import { PencilLine } from "phosphor-react"
+
 import styles from './Sidebar.module.css'
 
 export function Sidebar() {
@@ -10,12 +12,15 @@ export function Sidebar() {
             />
 
             <div className={styles.profile}>
+                <img className={styles.avatar} src="https://github.com/tuiusx.png" />
+
                 <strong>Ricardo Martins</strong>
                 <span>Web Developer</span>
             </div>
 
             <footer>
                 <a href="#">
+                    <PencilLine size={20} />
                     Editar seu perfil
                 </a>
             </footer>

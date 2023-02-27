af9469f7b74524b0b80d91acddbea0aff4366621
tuiusx <tuiusx@icloud.com>
2023-02-26 22:05:25 -0300

    Application structure - Component: Avatar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from "phosphor-react"
+import { Avatar } from "./Avatar"
 
 import styles from './Sidebar.module.css'
 
@@ -12,7 +13,7 @@ export function Sidebar() {
             />
 
             <div className={styles.profile}>
-                <img className={styles.avatar} src="https://github.com/tuiusx.png" />
+                <Avatar src="https://github.com/tuiusx.png" />
 
                 <strong>Ricardo Martins</strong>
                 <span>Web Developer</span>

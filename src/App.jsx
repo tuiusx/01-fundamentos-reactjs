909872812f88095d2f9fd947096104c6ec2169f7
tuiusx <tuiusx@icloud.com>
2023-02-27 14:18:55 -0300

    The React Engines - Iterating in JSX

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,40 @@ import styles from './App.module.css'
 
 import './global.css'
 
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/tuiusx.png',
+      name: "Ricardo Martins",
+      role: "Web Developer"
+    },
+    content: [
+      { type: 'paragraph', content: "Fala galeraa 👋" },
+      { type: 'paragraph', content: "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀", },
+      { type: 'link', content: "jane.design/doctorcare" },     
+    ],
+    publishedAt: new Date('2023-02-027 20:00:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://media.licdn.com/dms/image/C5603AQHVkJMUX_dyvg/profile-displayphoto-shrink_200_200/0/1516574696814?e=1683158400&v=beta&t=vvHJpHrQ-Y_Tz-lBbRVmXNU37kVGy-QSLtf5iDc2lWQ',
+      name: "Sarah",
+      role: "Medica"
+    },
+    content: [
+      { type: 'paragraph', content: "Fala galeraa 👋" },
+      { type: 'paragraph', content: "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀", },
+      { type: 'link', content: "jane.design/doctorcare" },     
+    ],
+    publishedAt: new Date('2023-02-30 20:00:00'),
+  },
+
+
+];
+
 export function App() {
 
   return (
@@ -14,14 +48,15 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-            <Post 
-              author="Ricador martins" 
-              content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit consectetur perferendis possimus pariatur sequi recusandae? Corporis fugit aliquid pariatur veritatis, ea sed dolore rerum nisi consequuntur nostrum, a doloribus recusandae!" 
-            />
-            <Post 
-              author="tiao" 
-              content="biscoito" 
-            />
+          {posts.map(post => {
+            return (
+              <Post 
+                author={post.author}
+                content= {post.content}
+                publishedAt= {post.publishedAt}
+              />
+            )
+          })}
         </main>
       </div>
 
